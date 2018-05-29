20bad4befd387a323d8632995fbbbefdc5dbc782
xiongjian <webjsforyou@gmail.com>
2018-05-29 12:05:20 +0800

    jquery中的ajax

--- NodeJS/day05/02.ajax.js
+++ NodeJS/day05/02.ajax.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/testAJAX', function (req, res) {
+  console.log(req.body);
+  res.send('接受ajax请求返回响应');
+})
+
 app.get('/testAJAX', function (req, res) {
+  console.log(req.query);
   res.send('接受ajax请求返回响应');
 })
 
