b3c85b9b678bd4baa4735c8ffb997b5a2e34c8e2
xiongjian <webjsforyou@gmail.com>
2018-06-11 15:48:05 +0800

    less css html

--- projectBuild/work/gulp_test/gulpfile.js
+++ projectBuild/work/gulp_test/gulpfile.js
@@ -4,12 +4,24 @@ var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var less = require('gulp-less');
+var LessAutoprefix = require('less-plugin-autoprefix');
+var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-htmlmin');
+var pkg = require('./package');   //引入package.json文件
+var jshintConfig = pkg.jshintConfig;  //拿到package.json中的属性
+
 
 //2. 配置/定义任务
+/*
+  回调函数中指定return的话  方法就是异步的
+  如果没有指定return  方法就是同步
+ */
 gulp.task('jshint', function () {   //语法检查的任务
   //任务的具体内容
   return gulp.src('src/js/*.js')   //将指定文件加载到内存中（数据流）
-    .pipe(jshint())   //语法检查
+    .pipe(jshint(jshintConfig))   //语法检查
     .pipe(jshint.reporter('default'))  //语法检查的报错规则
 })
 
@@ -22,6 +34,26 @@ gulp.task('js', ['jshint'], function () {   //['jshint']必须先执行，再执
     .pipe(gulp.dest('dist/js'))
 })
 
+gulp.task('less', function () {
+  return gulp.src('src/less/*.less')
+    .pipe(less({plugins: [autoprefix]}))
+    .pipe(rename('built.css'))
+    .pipe(gulp.dest('build/css'))
+})
+
+gulp.task('css', ['less'], function () {
+  return gulp.src('build/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))  //兼容ie8
+    .pipe(rename('dist.min.css'))
+    .pipe(gulp.dest('dist/css'))
+})
+
+gulp.task('html', function() {
+  return gulp.src('index.html')
+    .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
+    .pipe(gulp.dest('dist'));
+});
+
 
 //3. 应用任务
-gulp.task('default', ['js']);
+gulp.task('default', ['js', 'css', 'html']);
